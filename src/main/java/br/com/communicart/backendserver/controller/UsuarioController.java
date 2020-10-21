199141bb0c024e7bddb8da6e470f309742b7c037
Arthur Dias <arthurdias.trad@outlook.com>
2020-10-21 08:59:10 -0300

    criado PerfilController

--- src/main/java/br/com/communicart/backendserver/controller/UsuarioController.java
+++ src/main/java/br/com/communicart/backendserver/controller/UsuarioController.java
@@ -5,19 +5,14 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import br.com.communicart.backendserver.model.dto.CreatePessoaDTO;
 import br.com.communicart.backendserver.model.dto.UsuarioDTO;
 import br.com.communicart.backendserver.model.entity.Usuario;
-import br.com.communicart.backendserver.model.enums.TipoPessoa;
-import br.com.communicart.backendserver.service.PerfilService;
 import br.com.communicart.backendserver.service.UsuarioService;
 import lombok.AllArgsConstructor;
 
@@ -27,7 +22,6 @@ import lombok.AllArgsConstructor;
 public class UsuarioController {
 
 	private final UsuarioService usuarioService;
-	private final PerfilService perfilService;
 	
 	@PostMapping
 	public ResponseEntity<Void> create(@Valid @RequestBody UsuarioDTO usuarioDto) {
@@ -42,15 +36,5 @@ public class UsuarioController {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@PutMapping("/{idPerfil}/cadastro")
-	public ResponseEntity<Void> register(@PathVariable Long idPerfil, @RequestBody CreatePessoaDTO pessoaDto) {
-	
-		if (pessoaDto.getTipoPessoa().equals(TipoPessoa.PESSOA_FISICA)) {
-			this.perfilService.createPessoaFísica(pessoaDto, idPerfil);
-		} else if (pessoaDto.getTipoPessoa().equals(TipoPessoa.PESSOA_JURIDICA)) {
-			this.perfilService.createPessoaJuridica(pessoaDto, idPerfil);
-		}
-		
-		return ResponseEntity.noContent().build();
-	}
+
 }
